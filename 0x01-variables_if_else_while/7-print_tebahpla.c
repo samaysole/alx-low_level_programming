9eb4ed99e4e25b8ef3243d80755e78216d945b72
Samaybyte <samisole1996@gmail.com>
2022-03-11 14:57:50 -0800

    file renamed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,19 +5,19 @@
  * main - main block
  * Description: Print all lowercase alphabets in reverse 
  * followed by a new line
- * Return: 0
+ * Return": 0
  */
 int main(void)
 {
-        char c = 'z';
+	char c = 'z';
 
-        while (c >= 'a')
-        {
-                putchar(c);
-                c--;
-        }
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
