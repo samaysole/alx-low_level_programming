deb5cfcf580e1f21ca654c2706d55f5252f899cb
Samaybyte <samisole1996@gmail.com>
2022-03-11 15:26:21 -0800

    print comb betty style modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-        int n =0;
+        int n = 0;
 
         while (n < 10)
         {
                 putchar(48+n);
                 if (n != 9)
                 {
-                    putchar(',');
-                    putchar(' ');
-                }
+                	putchar(',');
+                	putchar(' ');
+		}
                 n++;
         }
 
