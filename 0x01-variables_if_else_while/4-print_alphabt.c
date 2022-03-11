cfa24f85ad66622c4cab506e66c349678cbaa126
Samaybyte <samisole1996@gmail.com>
2022-03-11 14:10:16 -0800

    print alphabt without e and q modified

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,16 @@
  * main - main block
  * Description: print all alphabets except e and q
  * Return: 0
- * */
+ **/
 int main(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		if (!( c == 'e' || c == 'q'))
+		if (c != 'e' && c != 'q'))
 		{
-		       	putchar(c);
+			putchar(c);
 		}
 		c++;
 	}
