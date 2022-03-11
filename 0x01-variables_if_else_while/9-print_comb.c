29c4c7abaf00e2ee80980c8ebd165488e6710bf4
Samaybyte <samisole1996@gmail.com>
2022-03-11 15:20:24 -0800

    print comb added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digits base 10 
+ * followed by a comma, space and new line 
+ * Return: 0
+ */
+int main(void)
+{
+        int n =0;
+
+        while (n < 10)
+        {
+                putchar(48+n);
+                if (c != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+                n++;
+        }
+
+        putchar('\n');
+        return (0);
+}
+
