e33d5c8e408448db2dff61fff00009b19e7ac79e
Samaybyte <samisole1996@gmail.com>
2022-03-11 16:08:09 -0800

    hexadecimal code added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+        int n = 0 ;
+
+        while (n < 16)
+        {
+		if (n < 10)
+		{
+			putchar(48 + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
+		n++;
+	}
+
+        putchar('\n');
+        return (0);
+}
