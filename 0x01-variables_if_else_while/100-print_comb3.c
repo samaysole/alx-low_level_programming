3d7492c62c792c740965172ac8a77f1c56738eb4
Samaybyte <samisole1996@gmail.com>
2022-03-13 01:39:32 -0800

    comb3 code added)

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+	
+	n=0;
+
+	while (n < 9)
+	{
+		m = n + 1;
+		while (m < 10)
+		{
+			putchar(48 + n);
+			putchar(48 + m);
+
+			if (n + m != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return(0);
+}
