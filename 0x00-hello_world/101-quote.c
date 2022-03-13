12e3e825533a818a9ae9897f38ed0066bf76c1d4
Samaybyte <samisole1996@gmail.com>
2022-03-13 05:21:01 -0700

    quote code added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
+{
+	int n;
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	n = 0;
+	while (n < sizeof(ch))
+	{
+		putchar(ch[n]);
+		n++;
+	}
+	putchar('\n');
+	return (1);
+}
