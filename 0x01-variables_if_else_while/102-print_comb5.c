54e86448e84839c33fc07eff49aa5c64fe99f13b
Samaybyte <samisole1996@gmail.com>
2022-03-13 15:01:54 -0700

    two two digit code added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all possible different combinations of two two digits
+ * starting from 00 01 - 98 99 - "00 01 and 01 00" are considered as same numbers 
+ * of 0 and 1 and finally followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int l,m,n,o;
+
+	l=0;
+	while (l < 10)
+	{
+		m = 0;
+		while (m < 10)
+		{
+			n = 0;
+			while (n < 10)
+			{
+				o = 0;
+				while (o < 10)
+				{
+					if (!(l == m && o == n))
+					{
+						putchar(48 + l);
+						putchar(48 + m);
+						putchar(' ');
+						putchar(48 + n);
+						putchar(48 + o);
+
+						if ( l + m + n + o != 35)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					o++;
+				}
+				n++;
+			}
+			m++;
+		}
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
