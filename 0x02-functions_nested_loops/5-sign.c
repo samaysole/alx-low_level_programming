78a972c1de23e7b6a852a65ef4e2be64b5d136c7
Samaybyte <samisole1996@gmail.com>
2022-03-15 15:01:30 -0700

    sign betty styl modified

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 
 /**
  * print_sign - check for the value
- * @c : sign to check the value
+ * @n : sign to check the value
  * Return:0 if zero,  1 if greater than 0, or -1 if less than zero
  */
 
 int print_sign(int n)
 {
-	if (n >0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
