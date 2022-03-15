973d397745eae2b6fb9d0c17fe00c5cdd21f676c
Samaybyte <samisole1996@gmail.com>
2022-03-15 15:55:56 -0700

    every minute of a day code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - display the minutes of a day
+ * 
+ * Return:0
+ */
+
+void jack_bauer(void)
+{
+	int u, v, w, x;
+
+	for (u = 0; u <= 2; u++)
+	{
+		for (v = 0; v <= 3; v++)
+		{
+			for (w = 0; w<= 5; w++)
+			{
+				for (x = 0; x <= 9; x++)
+				{
+					_putchar(48 + u);
+					_putchar(48 + v);
+					_putchar(':');
+					_putchar(48 + w);
+					_putchar(48 + x);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
