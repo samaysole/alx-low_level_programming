b71a821d5d0504e247b7344425be49f74d468d04
Samaybyte <samisole1996@gmail.com>
2022-03-15 07:19:33 -0700

    putchar print modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,5 +7,15 @@
  **/
 int main(void)
 {
-	printf("_putchar\n");
+	int str[]= {95, 112, 117, 116, 99, 104, 97, 114}
+	int c, s;
+
+	s = sizeof(str) / sizeof(int);
+
+	for (c = 0; c < s; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
