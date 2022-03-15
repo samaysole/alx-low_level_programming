6397d41b09cc5e7991dbf510d4cd5e13d240d8fe
Samaybyte <samisole1996@gmail.com>
2022-03-15 16:33:26 -0700

    last digit code added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit from the given value
+ * Return: 0
+ */
+
+int print_last_digit(int l)
+{
+	int num, l;
+	
+	num = 897;
+	l = num % 10;
+	_putchar(48 + l);
+	_putchar('\n');
+}
