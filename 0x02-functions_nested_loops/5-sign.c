280225d6fecbf55de12761a3613b014c287513d8
Samaybyte <samisole1996@gmail.com>
2022-03-15 14:58:36 -0700

    sign code added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check for the value
+ * @c : sign to check the value
+ * Return:0 if zero,  1 if greater than 0, or -1 if less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
