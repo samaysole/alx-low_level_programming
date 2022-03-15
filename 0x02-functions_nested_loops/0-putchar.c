8785359e4544a21ca47d8be40e8c2635600252ce
Samaybyte <samisole1996@gmail.com>
2022-03-15 13:36:08 -0700

    putchar betty style modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int c[] = { 95, 112, 117, 116, 99, 104, 97, 114};
-        int n,s;
-       
-        s = sizeof(c) / sizeof(int);
-        for (n = 0; n < s; n++)
-        {
-	        _putchar(c[n]);
-        }
-        _putchar('\n');
-        return (0);
+	int n,s;
+	
+	s = sizeof(c) / sizeof(int);
+	for (n = 0; n < s; n++)
+	{
+		_putchar(c[n]);
+	}
+	_putchar('\n');
+	return (0);
 }
