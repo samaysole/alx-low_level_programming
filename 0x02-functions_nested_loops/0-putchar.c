4a3c3512dc6bcf7ac5e14f29865322cf00995f2d
Samaybyte <samisole1996@gmail.com>
2022-03-15 06:58:37 -0700

    putchar print code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print _putchar followd by new line
+ * Return: 0
+ **/
+int main(void)
+{
+	printf("_putchar\n");
+}
