216adbf8caa2959ae5e646258267594b6dd1262e
Samaybyte <samisole1996@gmail.com>
2022-03-15 11:43:05 -0700

    putchar cod modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  **/
 int main(void)
 {
-	char c[9] = "_putchar";
-	int n;
-
-	n = 0;
-	while (n < 9)
-	{
-		_putchar(c[n]);
-		n++;
-	}
-	_putchar('\n');
-	return (0);
+       int c[] = "_putchar";
+       int n,s;
+       
+       s = sizeof(c) / sizeof(int);
+       for (n = 0; n < s; n++)
+       {
+	       _putchar(c[n]);
+	       n++;
+       }
+       _putchar('\n');
+       return (0);
 }
