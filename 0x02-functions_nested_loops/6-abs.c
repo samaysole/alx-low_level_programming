a5ff5a10e3a3db4f93047477f52423b60c3b35ee
Samaybyte <samisole1996@gmail.com>
2022-03-15 15:12:22 -0700

    abs value code added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value integer calculator
+ * @i : integer value
+ * Return: Absolute value
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
