8f7acf64d1dec57cc557c9c26de214c6ab85c3cd
Samaybyte <samisole1996@gmail.com>
2022-03-16 12:31:24 -0700

    add code modified

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - adds two integers
- * 
+ * @x & @y: are the two integers
+ * @n : adds the value of the two integers
  * Return: returns added result
  */
 
@@ -11,5 +12,5 @@ int add(int x, int y)
 	int n;
 
 	n = x + y;
-	return n;
+	return (n);
 }
