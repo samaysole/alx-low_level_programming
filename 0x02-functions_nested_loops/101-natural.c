511a4e2a54cef15f121858010dfdb8e60f306e29
Samaybyte <samisole1996@gmail.com>
2022-03-16 15:49:17 -0700

    betty style modifeid

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block
- * Description: add all natural number of sum of 
+ * Description: add all natural number of sum of
  * all multiples of 3 & 5 below 1024
  * Return: 0 Always success
  */
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int sum, i;
-	
 	sum = 0;
 	for (i = 1; i < 1024; i++)
 	{
