d762d5f887cd4bde990b45058623cf5c576119db
Samaybyte <samisole1996@gmail.com>
2022-03-16 15:01:13 -0700

    98 code added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,72 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_to_98 - print all natural numbers from n to 98
- * @n : the natural number
- * Return: 0
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
  */
 
 void print_to_98(int n)
 {
-	int i, m, x, l;
-	if (n < 98)
+	while (n < 98)
 	{
-		for (i = 0; i <= 98; i++)
-		{
-			if (i < 0)
-			{
-				_putchar(45);
-				if (-1 * i / 10 != 0)
-				{
-					_putchar(-1 * i / 10 + 48);
-				}
-				_putchar(-1 * i % 10 + 48);
-			}
-			else
-			{
-				if (i / 10 != 0)
-				{
-					_putchar(i / 10 + 48);
-				}
-				_putchar(i % 10 + 48);
-			}
-			if (i < 98)
-			{
-				_putchar(44);
-				_putchar(32);
-			}
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	else if (n > 98)
+	while (n > 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i < 100)
-			{
-				_putchar(i / 10 + 48);
-				_putchar(i % 10 + 48);
-			}
-			else
-			{
-				m = i / 100;
-				_putchar(m + 48);
-				x = 100 * m;
-				l = i - x;
-				_putchar(l / 10 + 48);
-				_putchar(l % 10 + 48);
-			}
-			if (i > 98)
-			{
-				_putchar(44);
-				_putchar(32);
-			}
-		}
+		printf("%i, ", n);
+		n--;
 	}
-	else
-	{
-		_putchar(i / 10 + 48);
-		_putchar(i % 10 + 48);
-	}
-	_putchar('\n');
-	return 0;
+	printf("98");
+	putchar('\n');
 }
