79a82558c5b83578e2301db17361484d6f08fc10
Samaybyte <samisole1996@gmail.com>
2022-03-16 15:46:09 -0700

    betty style modifeid

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main : add all natural number of sum of all mltiples of 3 & 5 below 1024
- * 
+ * main : main block
+ * Description: add all natural number of sum of all multiples of 3 & 5 below 1024
  * Return: 0 Always success
  */
 
 int main(void)
 {
 	int sum, i;
+	
 	sum = 0;
 	for (i = 1; i < 1024; i++)
 	{
