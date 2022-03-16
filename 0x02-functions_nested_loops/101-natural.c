0a54029d5e5801d801b5c40d93c7fcf5ebc1d0d5
Samaybyte <samisole1996@gmail.com>
2022-03-16 15:39:21 -0700

    natural number modifeid

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@
 
 int main()
 {
-	int i;
-	
+	int sum, i;
+		
+	sum = 0;
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("%d \n", i);
+			sum += i;
 		}
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return 0;
 }
