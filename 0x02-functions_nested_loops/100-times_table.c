cb20c4894de4634bfc7e27684d803c7041097a93
Samaybyte <samisole1996@gmail.com>
2022-03-16 15:35:00 -0700

    natural number adde

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table of 9
+ * Return: void (0) Always success
+ */
+
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int op;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			op = a * b;
+			if (b == 0)
+			{
+				_putchar('0' + op);
+			}
+			else if (op < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + op);
+			}
+			else
+			{
+				_putchar('0' + op / 10);
+				_putchar('0' + op % 10);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* main : main blok that proints all the natural number
+ * of the sum of all mltiples of 3 & 5 below 1024
+ * Return: 0 Always success
+ */
+
+int main()
+{
+	int n, i;
+
+	n = 1024;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			printf("%d \n", i);
+		}
+	}
+	printf("\n");
+	return 0;
+}
