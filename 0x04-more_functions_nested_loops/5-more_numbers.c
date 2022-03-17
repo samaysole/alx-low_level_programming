8e2deec5439b9e92d25b95a04b4e60f1e2aa2852
Samaybyte <samisole1996@gmail.com>
2022-03-17 16:36:12 -0700

    more numbers added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ * Description: print all numbers from 0 to 14.
+ * Return: 0 always
+ */
+
+void more_numbers(void)
+{
+	char n, c;
+	int i = 0;
+
+
+	while (i < 10)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+
+			_putchar('0' + c);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}			
