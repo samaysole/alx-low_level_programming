70ce73296f36cf334aa7746588b22825d1d7f16f
Samaybyte <samisole1996@gmail.com>
2022-03-17 15:51:59 -0700

    multiplication code added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integer
+ * @a : the first integer
+ * @b : the second integer
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
