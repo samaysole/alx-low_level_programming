2bbb920ffdff74c5e92505e0770fb7fcdad76d87
Samaybyte <samisole1996@gmail.com>
2022-03-17 16:02:53 -0700

    print numbers code added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - print numbers from 0 to 9
+ * print_numbers - print numbers from 0 to 9
  * Return: 0 always
  */
 
@@ -13,6 +13,7 @@ void print_numbers(void)
 	while (i <= 9)
 	{
 		_putchar(48 + i);
+		i++;
 	}
 	_putchar('\n');
 	return (0);
