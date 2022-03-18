164249cd201502a6e470f51ff36428ed1968d867
Samaybyte <samisole1996@gmail.com>
2022-03-17 17:24:03 -0700

    square added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_square(int n)
+{
+	int r, c;
+
+	for (r = 0; r < size; r++)
+	{
+		for (c = 0; c < size; c++)
+		{
+			_putchar(35);
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
