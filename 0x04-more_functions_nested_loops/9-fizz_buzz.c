ed7bd59b6d51f5b99627ab4344b2f7997e22d1e1
Samaybyte <samisole1996@gmail.com>
2022-03-17 17:11:46 -0700

    Fizz Buzz Code modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,22 +18,24 @@ int main(void)
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (n % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", n);
-			printf(" ");
+		}
+		if (n != 100)
+		{
+			putchar(' ');
 		}
 	}
+	putchar('\n');
+	return (0);
 }
