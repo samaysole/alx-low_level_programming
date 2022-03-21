b5b6c37213f1839e7a71df3e822ec20c634fb23a
Samaybyte <samisole1996@gmail.com>
2022-03-21 14:38:56 -0700

    file added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - is a function that resets int value to 98
+ *
+ * Return: resets the int value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}	
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This project is done for -
+Pointers, Arrays and Strings
