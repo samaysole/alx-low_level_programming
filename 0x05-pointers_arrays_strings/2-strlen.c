3a38d50e26336f330368da14741c5bd67f76a305
Samaybyte <samisole1996@gmail.com>
2022-03-21 15:00:53 -0700

    strlen added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*(s + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
