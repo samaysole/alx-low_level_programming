f48d4ac702a94732349a654e718853fef26b464f
Samuel Solomon <85625481+Samaybyte@users.noreply.github.com>
2022-03-28 22:38:59 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - fills memory with a constant byte,
  * @s: memory area.
@@ -12,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(s + i) = b;
+		*(s + i) =  b;
 
 	return (s);
 }
