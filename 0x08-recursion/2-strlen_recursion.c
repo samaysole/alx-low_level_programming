9f01e197faf861abc5bb8726101f185f8ad536cb
Samaybyte <samisole1996@gmail.com>
2022-03-30 12:47:14 -0700

    strln recursion added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the length of the given string
+ * @s: input string
+ * Return: returns the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c = 1;
+
+	if (s[c] == '\0')
+	{
+		return 1;
+	}
+	return (c + _strlen_recursion(s + 1));
+}
