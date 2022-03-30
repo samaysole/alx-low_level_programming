b6e78afa6f7392dbc61ebeb5388f4f35fe5186fd
Samaybyte <samisole1996@gmail.com>
2022-03-30 07:26:27 -0700

    puts recursion added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a recursion that prints a string
+ * @s: input string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int c = 0;
+	if (s[c] == '\0')
+		return;
+	_putchar(s[c]);
+	_puts_recursion(s+1);
+}
