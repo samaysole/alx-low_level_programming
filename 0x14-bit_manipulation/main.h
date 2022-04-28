4b7363d515c7ac7514d1e790e3b87a928deaa8c3
Samaybyte <samisole1996@gmail.com>
2022-04-28 14:39:49 -0700

    set bit added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_
-#define _MAIN__
+#define _MAIN_
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
