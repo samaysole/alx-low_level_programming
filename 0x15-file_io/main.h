c5fc8e53d65bf9a0c65755cb58a54d46d519a581
Samaybyte <samisole1996@gmail.com>
2022-05-06 07:11:24 -0700

    textfiel added

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
