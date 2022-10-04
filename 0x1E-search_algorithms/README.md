80bc0b5d5d7fabbb7fa8f13a20ce0a56b216d894
skahsay <skahsay@paga.com>
2022-10-04 22:48:09 +0300

    added

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+# C - Search Algorithms
+
+All the files are compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
---
+++ 0x1E-search_algorithms/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *a, int i, int j);
+
+#endif
